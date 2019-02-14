e8473f714d66055b67e1357710c44ab35ab0e014
Hana <hanabananak@gmail.com>
2019-02-14 12:44:57 -0600

    Added files to the porject file

---
+++ src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-14
+OU ID_______________: 113327542
+OU EMAIL____________: hana.k.stevenson-1@ou.edu
+
+Tasks:
+1. XXX
+2. XXX
+
+Estimated Time (minutes):
+1. XXX
+2. XXx
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+*/}
